991e527e7b5624389728390fa3a6c7ddfed84662
LittleSound <464388324@qq.com>
2022-04-16 18:56:48 +0800

    ✨ Use the API while developing

--- src/components.d.ts
+++ src/components.d.ts
@@ -5,7 +5,6 @@ import '@vue/runtime-core'
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
-    ClientOnly: typeof import('vite-ssr')['ClientOnly']
     Counter: typeof import('./components/Counter.vue')['default']
     Footer: typeof import('./components/Footer.vue')['default']
     README: typeof import('./components/README.md')['default']
